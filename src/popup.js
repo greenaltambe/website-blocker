1010092d6f2af23507839a1117ccee6fa986a7f6
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-07-15 19:29:12 +0530

    Add block sites list and remove site button

--- src/popup.js
+++ src/popup.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	const addCurrentSiteButton = document.getElementById(
 		"add-current-site-btn"
 	);
+	const blockedSitesList = document.getElementById("blocked-sites-list");
 
 	// Add current site to input
 	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -18,6 +19,41 @@ document.addEventListener("DOMContentLoaded", () => {
 		addCurrentSiteInput.value = hostname;
 		addCurrentSiteInput.disabled = true;
 	});
+
+	function addSiteToList(site, blockedSites) {
+		const li = document.createElement("li");
+		const div = document.createElement("div");
+		const input = document.createElement("input");
+		const button = document.createElement("button");
+
+		div.classList.add("remove-site-container");
+		input.type = "text";
+		input.value = site;
+		input.disabled = true;
+		button.classList.add("remove-site");
+		button.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+      <title>close-thick</title>
+      <path d="M20 6.91L17.09 4L12 9.09L6.91 4L4 6.91L9.09 12L4 17.09L6.91 20L12 14.91L17.09 20L20 17.09L14.91 12L20 6.91Z"/>
+    </svg>
+  `;
+
+		button.addEventListener("click", () => {
+			const index = blockedSites.indexOf(site);
+			if (index > -1) {
+				blockedSites.splice(index, 1);
+				sitesInput.value = blockedSites.join(", ");
+				li.remove();
+				status.textContent = "Site removed";
+				setTimeout(() => (status.textContent = ""), 2000);
+			}
+		});
+
+		li.appendChild(div);
+		div.appendChild(input);
+		div.appendChild(button);
+		blockedSitesList.appendChild(li);
+	}
 
 	// Add current site
 	addCurrentSiteButton.addEventListener("click", () => {
@@ -34,7 +70,20 @@ document.addEventListener("DOMContentLoaded", () => {
 			} else {
 				blockedSites.push(site);
 				sitesInput.value = blockedSites.join(", ");
+				addSiteToList(site, blockedSites);
+				status.textContent = "Site added";
+				setTimeout(() => (status.textContent = ""), 2000);
 			}
+		});
+	});
+
+	// Add all sites to list
+	chrome.storage.local.get("blockedSites", (data) => {
+		const blockedSites = data.blockedSites || [];
+
+		// create li items for each sites
+		blockedSites.forEach((element) => {
+			addSiteToList(element, blockedSites);
 		});
 	});
 
